4f3727cca33540965e8e0d30b9e69ccdce2d05a1
mayunfei <mmayunfei@163.com>
2026-04-15 17:35:45 +0800

    update

--- server.bat
+++ server.bat
@@ -1,13 +1,6 @@
-@echo off
-cd /d "%~dp0"
-
-if not exist cloudflared.exe (
-    echo 正在下载 cloudflared...
-    powershell -Command "Invoke-WebRequest -Uri https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe -OutFile cloudflared.exe"
-)
-
-echo 正在启动 Cloudflare Tunnel...
-echo 外网地址会显示在下面
-cloudflared.exe tunnel --url http://localhost:8080
+﻿
+echo Starting Cloudflare Tunnel...
+echo Using the website below
+cloudflared tunnel --url http://127.0.0.1:8080
 
 pause
